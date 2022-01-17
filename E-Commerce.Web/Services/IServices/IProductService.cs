89051f47a3b77f81e4d0a3bcd05dca5459f4d713
rafaelalvesmds <rafaelalvesmds.dev@gmail.com>
2022-01-17 20:27:28 -0300

    Implentando Classe ProductService

--- E-Commerce.Web/Services/IServices/IProductService.cs
+++ E-Commerce.Web/Services/IServices/IProductService.cs
@@ -5,7 +5,7 @@ namespace E_Commerce.Web.Services.IServices
     public interface IProductService
     {
         Task<IEnumerable<ProductModel>> FindAllProducts();
-        Task<ProductModel> FindAProductsById(long id);
+        Task<ProductModel> FindProductById(long id);
         Task<ProductModel> CreateProduct(ProductModel model);
         Task<ProductModel> UpdateProduct(ProductModel model);
         Task<bool> DeleteProductById(long id);
