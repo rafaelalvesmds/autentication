42b800f2f67a75a894e64b7532e00a03a729b2d8
rafaelalvesmds <rafaelalvesmds.dev@gmail.com>
2022-01-17 16:53:14 -0300

    Criando Controller [GET, POST, PUT, DELETE]

---
+++ E-Commerce.API/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using E_Commerce.API.Data.ValueObjects;
+using E_Commerce.API.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new
+                ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll(long id)
+        {
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+            var product = await _repository.Create(vo);
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+            var product = await _repository.Update(vo);
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+            if (!status) return BadRequest();
+            return Ok(status);
+        }
+
+    }
+}
