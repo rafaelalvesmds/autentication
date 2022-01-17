6e516d0060fbc39a81e23afe33bc83d4fc40f9f3
rafaelalvesmds <rafaelalvesmds.dev@gmail.com>
2022-01-17 16:45:55 -0300

    Implementando Value Object

---
+++ E-Commerce.API/Data/ValueObjects/ProductVO.cs
@@ -0,0 +1,12 @@
+﻿namespace E_Commerce.API.Data.ValueObjects
+{
+    public class ProductVO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string? CategotyName { get; set; }
+        public string? imageUrl { get; set; }
+    }
+}
---
+++ E-Commerce.API/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using E_Commerce.API.Data.ValueObjects;
+
+namespace E_Commerce.API.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(long id);
+    }
+}
