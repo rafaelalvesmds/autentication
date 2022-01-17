f3ab482f61b79016aba6104ea35f5fe70d4c6f26
rafaelalvesmds <rafaelalvesmds.dev@gmail.com>
2022-01-17 16:38:00 -0300

    Entidade, Base de Dados Criadas // Migration Executada

--- E-Commerce.API/Program.cs
+++ E-Commerce.API/Program.cs
@@ -1,6 +1,13 @@
+using E_Commerce.API.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<MySQLContext>
+    (options => options.UseMySql("Server=localhost; DataBase=shopping_product_api;Uid=root;Pwd=admin123",
+    Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.27-mysql")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
